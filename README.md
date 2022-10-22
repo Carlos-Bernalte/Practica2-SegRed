ee7f476ca0299ad7efaa3d89f19d3f4d0f38c09c
carlos_bernalte <carlos.bernalte@alu.uclm.es>
2022-10-22 03:51:08 +0200

    Scripts added

---
+++ README.md
@@ -0,0 +1,22 @@
+# Practica 2
+
+## Enunciado
+
+Se debe realizar un programa que averigüe la clave con la que se cifró el archivo dado. Este programa se puede realizar en cualquier lenguaje de programación. Se debe incluir en el entregable:
+
+- Un README explicando cómo instalar y ejecutar el programa en un entorno GNU/Linux.
+- El código fuente del programa.
+
+Se valorará:
+
+- La rapidez con la que se encuentra la clave: cuanto más rápido mejor.
+- La limpieza del código y del contenido del proyecto.
+
+# Ejecuciones
+
+| Número de procesos | Min longitud | Max longitud | Combinaciones posibles | Tiempo de ejecución |
+|--------------------|--------------|--------------|------------------------|---------------------|
+| 8                  | 1            | 5            | 12356630               |                     |
+| 8                  | 4            | 4            | 456976                 | 5:58:40             |
+| 16                 | 1            | 5            | 12356630               |             |
+| 16                 | 4            | 4            | 456976                 | 1:45:58             |
---
+++ pass_solver.py
@@ -0,0 +1,67 @@
+##!/usr/bin/env python3
+
+import sys
+import subprocess
+import time
+import argparse
+from multiprocessing import Process, cpu_count, Value
+
+
+ARCHIVO = 'archivo.pdf.gpg' 
+founded = Value('i', False)
+
+def pass_solver(i, file):
+    try:
+        start_time = time.time()
+        with open('diccionarios/dicc_'+str(i)+'.txt', 'r') as fichero:
+            for password in fichero:
+                if founded.value:
+                    break
+                password = password.rstrip('\n')
+                if(check_pass(password, file)):
+                    end_time = time.time()
+                    total_time = end_time - start_time
+                    print('La clave es: {}'.format(password))
+                    open('password.txt', 'a').write('Contraseña: '+password +'\n'+calc_hora(total_time)+'\n')
+                    sys.exit()
+    except KeyboardInterrupt:
+        print('Saliendo...')
+        sys.exit()
+    
+def check_pass(password, file):
+    try:
+        # subprocess.check_output(['gpg', '--batch', '--passphrase', password, '-d', ARCHIVO, '>>', 'resultado.pdf'], stderr=subprocess.DEVNULL)
+        subprocess.check_output(['gpg', '--batch', '--passphrase', password, '-d', file], stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        return False
+    except KeyboardInterrupt:
+        sys.exit()
+    else:
+        founded.value = True
+        return True
+
+def calc_hora(segundos):
+    horas = segundos // 3600
+    segundos %= 3600
+    minutos = segundos // 60
+    segundos %= 60
+    return 'Tiempo de ejecucion: '+str(int(horas))+':'+str(int(minutos))+':'+str(int(segundos))
+
+def args_parser():
+    parser = argparse.ArgumentParser(description='Script para descifrar un archivo con gpg')
+    parser.add_argument('-f', '--file', type=str, help='Archivo a descifrar', required=True)
+    parser.add_argument('-n', '--n_processes', type=int, default=cpu_count(), help='Numero de procesos a lanzar')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = args_parser()
+    for i in range(1, args.n_processes+1):
+        Process(target=pass_solver, args=(i,args.file)).start()
+    
+
+
+
+
+
+    
